262632f6fcc030e44596b8592e6c1721df84e0e1
Xavier Fernandes <fernandes.praphat@gmail.com>
2020-06-15 12:07:46 -0700

    Add storage provider tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,8 @@
 class TestBase(object):
 
+    def assertTrue(self, value):
+        assert(value)
+
     def assert_keys(self, dictionary, keys):
         for key in keys:
             assert(key in dictionary)
