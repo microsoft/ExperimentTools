66be2b0bacf2b35e00f54c7359fbe9d3b78887f1
Xavier Fernandes <fernandes.praphat@gmail.com>
2020-06-29 12:53:08 -0700

    Create resources folder

--- prepare-test-environment.py
+++ prepare-test-environment.py
@@ -7,6 +7,7 @@ from xtlib.helpers import xt_config
 def place_merged_config():
     resources_folder = xt_config.get_resource_dir()
     config_string = base64.b64decode(os.environ.get("ENCODED_BASE_CONFIG")).decode("utf-8")
+    os.mkdirs(resources_folder)
     default_config_file = open(f"{resources_folder}/default_config.yaml", "w")
     default_config_file.write(config_string)
     default_config_file.close()
