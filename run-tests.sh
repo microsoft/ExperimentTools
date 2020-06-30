24a1c2757a6ceb2aa6c9d4a1f0db8822f7d2c28e
Xavier Fernandes <fernandes.praphat@gmail.com>
2020-06-30 13:41:47 -0700

    Attempt to run a local job

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,6 @@ pip install -r dev_requirements.txt
 pip install -e .
 python prepare-test-environment.py
 XT_GLOBAL_CONFIG=tests/xt_config.yaml xt help
-XT_GLOBAL_CONFIG=tests/xt_config.yaml xt run --target=aml $(System.DefaultWorkingDirectory)/tests/fixtures/miniMnist.py
+XT_GLOBAL_CONFIG=tests/xt_config.yaml xt run --target=local $(System.DefaultWorkingDirectory)/tests/fixtures/miniMnist.py
 # XT_GLOBAL_CONFIG=tests/xt_config.yaml pytest tests/ -s --junitxml=test-results.xml
 # XT_GLOBAL_CONFIG=tests/xt_config.yaml pytest tests/ -v --junitxml=test-results.xml
