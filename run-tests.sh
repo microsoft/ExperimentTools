505b8706e5fe2e6cb323b9c5008419ce68eabe56
Xavier Fernandes <fernandes.praphat@gmail.com>
2020-06-30 12:53:21 -0700

    Only run config tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 pip install -e .
 pip install -r dev_requirements.txt
 python prepare-test-environment.py
-XT_GLOBAL_CONFIG=tests/xt_config.yaml pytest tests/test_storage_provider.py -s
+XT_GLOBAL_CONFIG=tests/xt_config.yaml pytest tests/test_config.py -s
