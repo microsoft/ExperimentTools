a3fe43c110200607b21141fa7ad08558494b4ba6
Xavier Fernandes <fernandes.praphat@gmail.com>
2020-06-30 13:43:58 -0700

    List blobs

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,7 @@ pip install -e .
 python prepare-test-environment.py
 XT_GLOBAL_CONFIG=tests/xt_config.yaml xt help
 ls -l tests/fixtures
+XT_GLOBAL_CONFIG=tests/xt_config.yaml xt list blobs
 XT_GLOBAL_CONFIG=tests/xt_config.yaml xt run --target=local tests/fixtures/miniMnist.py
 # XT_GLOBAL_CONFIG=tests/xt_config.yaml pytest tests/ -s --junitxml=test-results.xml
 # XT_GLOBAL_CONFIG=tests/xt_config.yaml pytest tests/ -v --junitxml=test-results.xml
