0f62591929fe16059d45d2c126f042cc9bac60c9
Xavier Fernandes <fernandes.praphat@gmail.com>
2020-07-01 14:17:15 -0700

    Run all tests

--- run-tests.sh
+++ run-tests.sh
@@ -2,4 +2,4 @@ pip install -r dev_requirements.txt
 pip install -e .
 python prepare-test-environment.py
 XT_GLOBAL_CONFIG=tests/xt_config.yaml xt help
-XT_GLOBAL_CONFIG=tests/xt_config.yaml pytest tests/test_feature.py -v -s --junitxml=test-results.xml
+XT_GLOBAL_CONFIG=tests/xt_config.yaml pytest tests/ -v -s --junitxml=test-results.xml
