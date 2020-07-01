a465d48ff7e3e85f038a2d1da8a39cdab2b55116
Xavier Fernandes <fernandes.praphat@gmail.com>
2020-07-01 12:42:26 -0700

    Have AML auth use the service principal

--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,6 @@ pip install -r dev_requirements.txt
 pip install -e .
 python prepare-test-environment.py
 XT_GLOBAL_CONFIG=tests/xt_config.yaml xt help
-ls -l tests/fixtures
 XT_GLOBAL_CONFIG=tests/xt_config.yaml python -c 'from xtlib import xt_cmds; xt_cmds.main("xt run --target=aml tests/fixtures/miniMnist.py")'
 # XT_GLOBAL_CONFIG=tests/xt_config.yaml xt list blobs
 # XT_GLOBAL_CONFIG=tests/xt_config.yaml xt run --target=local tests/fixtures/miniMnist.py
