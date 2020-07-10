5e697e1328d28f22bd7ffda7d07985242eecffee
ilmarinen <fernandes.praphat@gmail.com>
2020-07-10 11:35:15 -0700

    Allow Azure Pipeline to use the GCR VM and enable Philly tests to run automatically on the GCR VM, triggered by the pipeline (#10)

--- prepare-test-environment.py
+++ prepare-test-environment.py
@@ -7,7 +7,8 @@ from xtlib.helpers import xt_config
 def place_merged_config():
     resources_folder = xt_config.get_resource_dir()
     config_string = base64.b64decode(os.environ.get("ENCODED_BASE_CONFIG")).decode("utf-8")
-    os.makedirs(resources_folder)
+    if not os.path.exists(resources_folder):
+        os.makedirs(resources_folder)
     default_config_file = open(f"{resources_folder}/default_config.yaml", "w")
     default_config_file.write(config_string)
     default_config_file.close()
--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,14 @@
 pip install -r dev_requirements.txt
 pip install -e .
-python prepare-test-environment.py
-XT_GLOBAL_CONFIG=tests/xt_config.yaml xt help
+
+# if [ "$PHILLY_TESTS" = "true" ]; then
+#    pip install xtlib-internal -i https://pkgs.dev.azure.com/msresearch/e709de22-dd8c-4b66-a84e-688f2a391d01/_packaging/eXperimentTools/pypi/simple/
+#    xtlib-internal config --reset
+# fi
+
+# pip install keyring artifacts-keyring
+# python prepare-test-environment.py
+# XT_GLOBAL_CONFIG=tests/xt_config.yaml xt help
 XT_GLOBAL_CONFIG=tests/xt_config.yaml pytest tests/ -v --junitxml=test-results.xml
+# XT_GLOBAL_CONFIG=tests/xt_config.yaml xt run --target=philly tests/fixtures/miniMnist.py
+# curl https://philly
